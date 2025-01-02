9a3adfb5fe7630cff9090473f7e38a204ce0205c
Noy Amsalem <noyamsalem@Users-MBP.lan>
2025-01-02 17:29:13 +0200

    asd

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express'); // ייבוא ספריית Express
+const mongoose = require('mongoose'); // ייבוא ספריית Mongoose לחיבור למסד נתונים
+const bodyParser = require('body-parser'); // ספרייה לטיפול בבקשות JSON
+
+// יצירת אפליקציית Express
+const app = express();
+app.use(bodyParser.json()); // Middleware לטיפול בבקשות JSON
+
+// חיבור ל-MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/rest-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+});
+
+// ייבוא ראוטים
+const postsRoutes = require('./routes/postsRoutes'); // נתיבים לפוסטים
+const commentsRoutes = require('./routes/commentsRoutes'); // נתיבים לתגובות
+
+// רישום הנתיבים
+app.use('/post', postsRoutes); // כתובת בסיסית לפוסטים
+app.use('/comment', commentsRoutes); // כתובת בסיסית לתגובות
+
+// הפעלת השרת
+const PORT = 3000; // הפורט עליו השרת יפעל
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
