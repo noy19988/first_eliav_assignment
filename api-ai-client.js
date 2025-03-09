3cd24a0d776dbab3d71f1fd705fe71054dfae038
noy19988 <noyamsallem199@gmail.com>
2025-03-09 23:45:36 +0200

    with gimini calories

--- api-ai-client.js
+++
@@ -1,9 +0,0 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-const genAI = new GoogleGenerativeAI("AIzaSyBb7FNvKSqrTXBNo6QbCpesS77Jg1R_8RQ");
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-const prompt = "Explain how AI works";
-
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
